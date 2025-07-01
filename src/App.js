b6a9a02d4eb6ed7e3fe29ffbb66a64f2bcdbd3e1
ARMA2025 <araibia.mohamed@gmail.com>
2025-07-01 09:24:48 +0100

    fix bugs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>ARMA Application Frontend</h1>
+      <p>This is a placeholder React app. Your interface will be implemented here.</p>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard/*" element={<Dashboard />} />
-    </Routes>
-  </Router>
-);
+root.render(<App />);
